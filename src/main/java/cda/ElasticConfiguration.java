16763b605cb8f4f771e70083cbff60ecbe1b4ea0
cdalxndr <cdalxndr@yahoo.com>
2022-08-15 21:03:36 +0300

    Initial commit

---
+++ src/main/java/cda/ElasticConfiguration.java
@@ -0,0 +1,15 @@
+package cda;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticConfiguration extends ElasticsearchConfiguration {
+	@Override
+	public ClientConfiguration clientConfiguration() {
+		return ClientConfiguration.builder() //
+				.connectedTo("localhost:9200") //
+				.build();
+	}
+}
---
+++ src/test/java/cda/MyTest.java
@@ -0,0 +1,15 @@
+package cda;
+
+import co.elastic.clients.elasticsearch._types.mapping.RuntimeFieldType;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MyTest {
+
+	@Test
+	void testStartup() {
+	}
+}
